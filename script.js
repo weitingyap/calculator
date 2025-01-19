90bd5e8571edb80f4332a4ddfd0d35e2f146ba6e
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-18 17:32:54 -0800

    feat: operate on two operands using given operator

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-function add(a, b){
-    return a+b;
-}
+// Apply math operations (+, -, /, *) to two operands
 
-function substract(a, b){
-    return a-b;
-}
+function operate(a, b, operator){
+    function add(a, b){
+        return a+b;
+    }
+    
+    function substract(a, b){
+        return a-b;
+    }
+    
+    function divide(a, b){
+        return a/b;
+    }
+    
+    function multiply(a, b){
+        return a*b;
+    }
 
-function divide(a, b){
-    return a/b;
-}
-
-function multiply(a, b){
-    return a*b;
+    switch (operator){
+        case '+':
+            return add(a,b);
+        case '-':
+            return substract(a,b);
+        case '/':
+            return divide(a,b);
+        case '*':
+            return multiply(a,b);
+    }
 }
