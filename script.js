c81224cbcf83e19a9ed15c38dbafa1c9e6a03862
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-19 20:57:24 -0800

    style: moved frontend-backend code to end for readability

--- script.js
+++ script.js
@@ -35,35 +35,6 @@ for (let i = 0; i < nRows; i++){
     }
 }
 
-// Register button input and display
-
-const screen = document.querySelector("#screen")
-
-function getButtonType(event){
-    const NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    const BINARY_OPERATORS = ['+', '-', '/', '*'];
-    const UNARY_OPERATORS = ['+/-', '%'];
-    const CANCEL = ['a/c'];
-    const EQUAL = ['=']; // can be a binary or unary operator
-
-    const innerText = event.target.innerText;
-
-    if (NUMBERS.includes(innerText)) return 'number';
-    if (BINARY_OPERATORS.includes(innerText)) return 'binary_operator';
-    if (UNARY_OPERATORS.includes(innerText)) return 'unary_operator';
-    if (CANCEL.includes(innerText)) return 'cancel';
-    if (EQUAL.includes(innerText)) return 'equal';
-}
-
-function updateScreen(event){
-    // Only number types are displayed on screen
-    // Note: number input acts as string concatenation
-    
-    if (getButtonType(event) === 'number') screen.innerText += event.target.innerText;
-}
-
-pad.addEventListener('click', updateScreen);
-
 // Apply math operations (+, -, /, *) to two operands
 
 function operate(operator, a, b){
@@ -106,3 +77,32 @@ function operate(operator, a, b){
             return changeSigns(a);
     }
 }
+
+// Register button input and display
+
+const screen = document.querySelector("#screen")
+
+function getButtonType(event){
+    const NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const BINARY_OPERATORS = ['+', '-', '/', '*'];
+    const UNARY_OPERATORS = ['+/-', '%'];
+    const CANCEL = ['a/c'];
+    const EQUAL = ['=']; // can be a binary or unary operator
+
+    const innerText = event.target.innerText;
+
+    if (NUMBERS.includes(innerText)) return 'number';
+    if (BINARY_OPERATORS.includes(innerText)) return 'binary_operator';
+    if (UNARY_OPERATORS.includes(innerText)) return 'unary_operator';
+    if (CANCEL.includes(innerText)) return 'cancel';
+    if (EQUAL.includes(innerText)) return 'equal';
+}
+
+function updateScreen(event){
+    // Only number types are displayed on screen
+    // Note: number input acts as string concatenation
+
+    if (getButtonType(event) === 'number') screen.innerText += event.target.innerText;
+}
+
+pad.addEventListener('click', updateScreen);
