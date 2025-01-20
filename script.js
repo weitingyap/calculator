ba5607c85c88c101bf63fa5ff9cb5f65a4f5ced3
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-19 19:27:12 -0800

    feat: add calculator buttons with styling
    Add calculator buttons using Javascript due to repetitive nature of
    creating. Added styling with standardized height and width. Buttons
    include:
    - 0 to 9
    - operators +, -, /, *, =
    - additional buttons: ac (clear), +/-, %

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// Create calculator buttons using loops because of repetitive nature
+
+const pad = document.querySelector('#pad');
+
+const padRow = document.createElement('div');
+padRow.classList.add('pad-row');
+
+const padBtn = document.createElement('button');
+padBtn.classList.add('pad-btn');
+
+const nBtnPerRow = 4;
+const nRows = 5;
+const calcButtons = [
+    ['AC', '+/-', '%', '/'],
+    ['7', '8', '9', '*'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    [' ', '0', '.', '=']
+]
+
+for (let i = 0; i < nRows; i++){
+    const newPadRow = padRow.cloneNode(false);
+    pad.appendChild(newPadRow);
+    for (let j = 0; j < nBtnPerRow; j++){
+        const newPadBtn = padBtn.cloneNode(false);
+        newPadBtn.innerText = calcButtons[i][j];
+
+        // blank extra button
+        if (newPadBtn.innerText === ' ') newPadBtn.disabled = true;
+
+        newPadRow.appendChild(newPadBtn);
+    }
+}
+
 // Apply math operations (+, -, /, *) to two operands
 
 function operate(a, b, operator){
